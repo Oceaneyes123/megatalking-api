975301655005610b9846fec2bd358d8142e49c84
dave <jezreldavesondia@gmail.com>
2020-12-02 07:08:19 +0800

    12-2

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
 const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
+
+//  const cors = require('cors');
+
 
 const app = express();
+// const server = require('http').createServer(app);
 
 
-app.use(cors());
+// app.use(cors());
 
 // Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.json())
 
-app.get('/',(req, res) => {
+app.use(express.json())
+// const WebSocket = require('ws')
+// const wss = new WebSocket.Server({ server });
+
+app.get('/api/next',(req, res) => {
     res.send("Please use POST request");
 });
 
-app.post('/',(req, res) => {
+app.post('/api/next',(req, res) => {
+    res.header("Content-Type", "application/json")
     const isNext = req.body;
-    res.send(isNext);
+    console.log(res)
+    res.json(isNext);
 });
+
+// wss.on('connection', function connection(ws) {
+//   console.log('A new client Connected!');
+//   ws.send('Welcome New Client!');
+
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
+
+//     ws.send('Hello World')
+
+//     wss.clients.forEach(function each(client) {
+//       if (client !== ws && client.readyState === WebSocket.OPEN) {
+//         client.send(message);
+//       }
+//     });
+    
+//   });
+// });
+
+app.get('/', (req, res) => res.send('Hello World!'))
 
 
 const port = process.env.PORT || 3000
